1fa53d722e7b13f223d8c8f7bf8ba85ec3ecc5de
Hemanth-Moka <2300030439@kluniversity.in>
2025-10-18 00:06:10 +0530

    base name

--- FRONTEND-PATIENT/vite.config.js
+++ FRONTEND-PATIENT/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-base :'/jenkinsfrontendapi/'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jenkinsfrontendapi/', 
   plugins: [react()],
 })
